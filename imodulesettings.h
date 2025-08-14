631f8ee56c6834085f11923a7f88246c7dc20efa
CharaVerKys <163038037+CharaVerKys@users.noreply.github.com>
2025-08-14 16:38:32 +0300

    Update imodulesettings.h

--- imodulesettings.h
+++ imodulesettings.h
@@ -4,23 +4,28 @@
 #include <defines.h>
 #include <qfile.h>
 
+#define VALIDATE_MODULE_PTR_TYPE \
+    assert(this); \
+    assert(0== std::strcmp(this->moduleName,MODULENAME_DEFINED));
+
 class IModuleSettings
 {
 public:
-    IModuleSettings(){}
+    IModuleSettings();
     virtual ~IModuleSettings(){}
     const char* getModuleName()const {assert(moduleName); return moduleName;}
-    SettingsModulesNames getModuleEnum()const {assert(moduleEnum != SettingsModulesNames::NotSetted); return moduleEnum;}
+    eSettingsModulesNames getModuleEnum()const {assert(moduleEnum != eSettingsModulesNames::eNotSetted); return moduleEnum;}
 
+    virtual bool isValidPtr() = 0;
     virtual bool readFromFile(QFile& stream);
-    void writeToFile(const std::string& path); // actually do nothing, only set target path
+    void writeToFile(std::string path); // actually do nothing, only set target path
     virtual QJsonObject getJson() const = 0;
     virtual bool setValuesOnJsonString(const char* jsonStr) = 0;
     virtual bool flush() const;
 
 protected:
     const char* moduleName = nullptr;
-    SettingsModulesNames moduleEnum = SettingsModulesNames::NotSetted;
+    eSettingsModulesNames moduleEnum = eSettingsModulesNames::eNotSetted;
     std::string path;
 };
 
