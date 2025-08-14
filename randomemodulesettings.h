ee27a78be80b762d5c2d0e64a435496ea6a3fc51
CharaVerKys <163038037+CharaVerKys@users.noreply.github.com>
2025-08-14 16:41:26 +0300

    Update randomemodulesettings.h

--- randomemodulesettings.h
+++ randomemodulesettings.h
@@ -13,6 +13,7 @@ public:
 
     QJsonObject getJson() const override{return {};}
     bool setValuesOnJsonString(const char* jsonStr) override{(void)jsonStr;return true;}
+    bool isValidPtr()override final;
 
     int savePeriod = 0;
     int timeCheckLogs = 0;
@@ -21,8 +22,17 @@ public:
 inline RandomModuleSettings::RandomModuleSettings()
 {
     moduleEnum = SettingsModulesNames::RandomName;
-    moduleName = "RandomModuleSettings";
+    moduleName = MODULENAME_DEFINED; // yap thats better
 }
+
+// that ideally should be in cpp file
+#define MODULENAME_DEFINED "RandomModuleSettings"
+include bool RandomModuleSettings::isValidPtr(){
+    VALIDATE_MODULE_PTR_TYPE
+    return 0== std::strcmp(this->moduleName,MODULENAME_DEFINED);
+}
+#indef MODULENAME_DEFINED
+//or you can def/undef in header
 
 
 #endif // LogUpdateModuleSettings_H
